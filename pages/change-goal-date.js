05fcf604d5111baef5a2795204cd98b683dec0ec
minifiedcode <minifiedcode@gmail.com>
2021-09-06 14:20:00 -0500

    Add state to the component to show visibility of the calendar container

--- pages/change-goal-date.js
+++ pages/change-goal-date.js
@@ -3,17 +3,39 @@ import Calendar from 'react-calendar'
 import styles from '../styles/GoalDate.module.scss'
 
 class ChangeGoalDate extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            calendarVisible: false
+        }
+    }
+
+    onClick = e => {
+        e.preventDefault()
+
+        this.setState({
+            calendarVisible: !this.state.calendarVisible
+        })
+    }
+
+    onChange = (value, event) => {
+        event.preventDefault()
+
+        console.log(value.toLocaleString('en-US'))
+    }
     render() {
         return (
             <div>
                 <h1>Change goal date</h1>
 
                 <div className={styles.dateInputContainer}>
-                    <input type="text" />
+                    <input type="text" className={styles.dateInput} />
                     <div>
-                        <button>Calendar</button>
+                        <button onClick={this.onClick}>Calendar</button>
                         <Calendar
-                            className={styles.calendarContainer}
+                            className={`${styles.calendarContainer} ${this.state.calendarVisible ? styles.visible : styles.hidden}`}
+                            // className={styles.calendarContainer, this.state.calendarVisible ? styles.visible : styles.hidden}
                             onChange={this.onChange}
                             value={new Date()}
                         />
